b3825ad3910a2b203d959feacceb9a2b0763cfe6
Dmytro Petrenko <blinker182ca@gmail.com>
2021-09-08 18:14:39 +0300

    add links for components

---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+export { default as Navigation } from './Navigation';
+export { default as PrivateRoute } from './PrivateRoute';
+export { default as PublicRoute } from './PublicRoute';
+export { default as ContactForm } from './Contacts/ContactForm';
+export { default as ContactList } from './Contacts/ContactList';
+export { default as Filter } from './Contacts/Filter';
---
+++ src/redux/phonebook/index.js
@@ -0,0 +1,4 @@
+export * as phoneActions from './phone-actions';
+export * as phoneOperations from './phone-operations';
+export { default as phoneReducers } from './phone-reducer';
+export * as phoneSelectors from './phone-selector';
